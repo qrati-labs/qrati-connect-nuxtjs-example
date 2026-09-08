06c61a69f319bafd0ceb15ab4b7f0f1df04c87ba
Siddhartha Maity <siddharthamaity1993@gmail.com>
2026-09-08 16:22:12 +0530

    fix: configure Nuxt Cloudflare deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2026-07-06',
   devtools: { enabled: false },
+  nitro: { preset: 'cloudflare_module' },
   app: { baseURL: '/connect/nuxtjs-example/' },
   css: ['~/assets/css/main.css'],
   runtimeConfig: { public: { organizationId: '', gtmId: '' } },
